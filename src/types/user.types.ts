255bdfb5258e037fb7a62d37416b7121414ab9cc
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-24 09:47:41 +0545

    type

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,8 +1,10 @@
 import {z} from 'zod';
-export const newUser = z.object({
+export const UserSchema = z.object({
     id: z.string().min(1,{message: "ID is required"}),
     username: z.string().min(1,{message: "Username is required"}),
     email: z.string().min(1,{message: "email is required"}),
     name: z.string().min(1,{message: "Name is required"}),  
-    age: z.string().optional();
+    age: z.string().optional()
 });
+
+export type User =z.infer<typeof UserSchema>;
