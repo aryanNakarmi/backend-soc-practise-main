6ac3c0b3135192466120df7c7e8c0827045922dd
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-25 21:21:25 +0545

    index

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ import bodyParser from 'body-parser';
 const app = express();
 app.use(bodyParser.json());
 
-
 const PORT = 3000;
+
+app.use("/api/users",us)
+
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
