77981df26e0abcbc4d7f013c22691efa4b7f34f7
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-25 21:28:05 +0545

    routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import router from './routes/user.routes';
 
 const app = express();
 app.use(bodyParser.json());
 
 const PORT = 3000;
 
-app.use("/api/users",us)
+app.use("/api/users",router);
 
 
 app.listen(PORT, () => {
